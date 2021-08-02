9361012a3c453941ccf7d2e80233ea51f9d5d48c
ToonHeleven <toon.heleven@student.uhasselt.be>
2021-08-02 12:17:51 +0200

    extra datasets

---
+++ eventlog/jsontocsv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+phone_usage_cleaned = pd.read_json(path_or_buf="phone_usage3.json")
+
+phone_usage_cleaned = phone_usage_cleaned[["header", "time"]]
+phone_usage_cleaned = phone_usage_cleaned.rename(columns={'header': 'Appname',
+                            'time': 'Timestamp'})
+phone_usage_cleaned = phone_usage_cleaned.iloc[::-1].reset_index(drop=True)
+phone_usage_cleaned['Timestamp'] = pd.to_datetime(phone_usage_cleaned['Timestamp'])
+phone_usage_cleaned['Timestamp'] = phone_usage_cleaned['Timestamp'].dt.strftime("%d/%m/%Y %X")
+
+phone_usage_cleaned.to_csv(path_or_buf="phone_usage_cleaned3.csv")
+
+print(phone_usage_cleaned.head())
